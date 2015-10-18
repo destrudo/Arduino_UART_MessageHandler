9c256a9572c4fdf8ad5f460c9118ebb6a690aa86
Destrudo Dole <destrudo@nulluniverse.com>
2015-10-18 04:04:58 -0700

    Building up libUARTMessageHandler...

--- controller/libUARTMessageHandler.py
+++ controller/libUARTMessageHandler.py
@@ -6,6 +6,32 @@
 ###############################################################################
 from __future__ import print_function
 
+def to_bytes(n, length, endianess='big'):
+	h = '%x' % n
+	s = ('0'*(len(h) % 2) + h).zfill(length*2).decode('hex')
+	return s if endianess == 'big' else s[::-1]
+
+def listOverlay(listBase, listAdd, offset)
+	for entry in range(0, len(listAdd)):
+		if (entry + offset) >= len(listBase): #safety incase of insanity
+			listBase.append(listAdd[entry])
+		else:
+			listBase[entry + offset] = listAdd[entry]
+
+	return listBase
+
+headerOffsets = {
+	"key":0,
+	"cmd_0":1,
+	"cmd_1":2,
+	"scmd":3,
+	"version":4,
+	"out_0":5,
+	"out_1":6,
+	"in_0":7,
+	"in_1":8,
+	"sum":9
+}
 
 # lrcsum
 #
@@ -30,13 +56,15 @@ class UART_Digital:
 			"pinm":0xff
 		}
 
-	#leds is a list of leds
-	def get(id, leds):
-		leds = None
-
-	#leds is a list of leds, colors is the set of colors to use
-	def set(id, leds, colors):
+	#pin is the pin
+	#pt is 0 for digital, 1 for analog
+	def get(pin, pt):
 		pass
+
+	#Same as the above, val is for pt=1 0->255, and for pt=0, 0 is low, >0 is high, pt=3 sets pinMode to val
+	def set(pin, pt, val):
+		pass
+
 
 class UART_Neopixel:
 	def __init__():
@@ -48,13 +76,42 @@ class UART_Neopixel:
 			"del":0xff
 		}
 
-	#pin is the pin
-	#pt is 0 for digital, 1 for analog
-	def get(pin, pt):
+		self.subcommandKeys = {
+			"ctrl":[ "id", "leds"], 
+			"ctrli":[ "id", "leds" ],
+			"clear":[ "id" ],
+			"add":[ "id", "pin", "len"],
+			"del":[ "id" ]
+		}
+
+		#data[data] should contain a dictionary containing `pixel:{color-red, color-green, color-blue}` sets
+
+		self.strips = {}
+
+
+	#leds is a list of leds
+	def get(id, leds):
+		leds = None
+
+	#leds is a list of leds, colors is the set of colors to use
+	#state set to anything other than 0 sets the subcmd to ctrli
+	def set(id, leds, colors, state=0):
 		pass
 
-	#Same as the above, val is for pt=1 0->255, and for pt=0, 0 is low, >0 is high
-	def set(pin, pt, val):
+	def clear(id):
+		pass
+
+	def add(curMsg, id, pin, length):
+		curMsg[headerOffsets["scmd"]] = self.subcommands[add]
+		curMsg[headerOffsets["out_0"]] = 0x01
+		self.strips[id] = { "pin":pin, "len":length }
+		#curMsg should now have a near-complete message
+		buf.append(
+
+	def delete(id):
+		pass
+
+	def prepareMessage(buffer, dataIn):
 		pass
 
 class UART_MH:
@@ -62,19 +119,25 @@ class UART_MH:
 		#Here we define a bunch of class variables
 		serialBaud = 115200
 
-		self.key = 0xAA
+		self.key = '\xaa'
 
 		self.commands = {
-			"mhconfig":0x00,	#messagehandler configuration command
-			"digital":0x01,		#Digital configuration command
-			"neopixel":0x02		#NeoPixel configuration command
+			"mhconfig":0x0000,	#messagehandler configuration command
+			"digital":0x0001,		#Digital configuration command
+			"neopixel":0x0002		#NeoPixel configuration command
+		}
+
+		#Command class definitions
+		self.commandC = {
+			"neopixel":UART_Neopixel(),
+			"digital":UART_Digital()
 		}
 
 		self.subcommands = {
 			"None":0x00
 		}
 
-		self.version = [ 0x00 ] #This variable must be adjusted to accomodate
+		self.versions = [ 0x00 ] #This variable must be adjusted to accomodate
 								# other compatible firmware versions.
 
 		#Header length info
@@ -83,9 +146,77 @@ class UART_MH:
 			"cmd":2,
 			"scmd":1,
 			"version":1
+			"out":2,
+			"in":2,
+			"sum":1
 		}
 
+		#Get the total header length based on the above framework.
+		self.headerlen = 0
+		for item in self.header:
+			self.headerlen+=self.header[item]
+
+		self.npmsg = UART_Neopixel()
+
 		self.ser = serial.Serial(str(serialInterface), serialBaud)
+
+		#At this point we'd be talking to the arduino and getting it's version,
+		# then compare it to the valid ones.
+		self.version = 0x00
+
+	#This prepares the initial message based on the main command type
+	def prepMessage(messageType):
+		outBuf = [
+			self.key,
+			'\x00',	#These are the two commands, they'll get set afterwards.
+			'\x00',	#cmd 1
+			'\x00',	#subcommand
+			to_bytes(self.version,1), #We always want to present the highest compat version.
+			'\x00',	#out 0
+			'\x00',	#out 1
+			'\x00',	#in 0
+			'\x00',	#in 1
+			'\x00'	#sum, needs to be here as a dummy for the classes to populate via .append()
+		]
+
+		#WE NEED EXCEPTIONS HERE
+		listOverlay(outBuf, to_bytes(self.commands[messageType], 2), 1)
+
+		#At this point, the message is just about as prepared as we can make it
+		# without the class-specific stuff populating the buffer.
+		return outBuf
+
+
+	#Compute the lrcsum for the message
+	def finishMessage(curMsg):
+		curMsg[9] = lrcsum(curMsg[:9])
+		return curMsg
+
+	def createMessage(dataIn):
+		# dataIn must be a dictionary which has these values set:
+		if "type" not in dataIn:
+			return 1
+		if dataIn["type"] not in self.commands:
+			return 1
+
+		#Make sure the command is in the class subcommands
+		if "command" not in dataIn:
+			return 2
+		if dataIn["command"] not in self.commandC[dataIn["type"]].subcommands:
+			return 2
+
+		#Make sure our data dict is configured
+		if "data" not in dataIn:
+			return 3
+
+		buf = prepMessage(dataIn["type"])
+		#Call the class-specific stuff.
+		buf = self.commandC[dataIn["type"]].prepareMessage(buf, dataIn)
+
+		#Finish it off
+		buf = finishMesage(buf)
+
+		return buf
 
 	#This sends the message
 	def sendMessage(buf):
@@ -97,3 +228,8 @@ class UART_MH:
 
 		#Right now, we're using a sleep.  In version 0x01 it'll be a set of 32 0x00's to end a group
 		time.sleep(0.15)
+
+#This will be the class to handle mqtt messages
+class UART_MH_MQTT:
+	def __init__():
+		pass
