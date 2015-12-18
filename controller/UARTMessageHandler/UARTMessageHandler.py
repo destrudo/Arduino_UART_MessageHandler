299ea6c86fb5bb854dc0aa5aebec17e56ccfd979
Destrudo Dole <destrudo@nulluniverse.com>
2015-12-18 00:08:09 -0800

    Significantly cleaned up UARTMessageHandler.py.  Now with additional globals and cleaned up exceptions.

--- controller/UARTMessageHandler/UARTMessageHandler.py
+++ controller/UARTMessageHandler/UARTMessageHandler.py
@@ -23,44 +23,11 @@ import socket
 import math #We needed the ceil() function.
 import multiprocessing
 
-#Debug value
+# Debug value
 DEBUG=0
-#Baud rate default value
+# Baud rate default value
 BAUD=250000
-#BAUD=115200
-
-def isInt(i):
-	try:
-		int(i)
-		return True
-	except ValueError:
-		return False
-
-def to_bytes(n, length, endianess='big'):
-	h = '%x' % n
-	s = ('0'*(len(h) % 2) + h).zfill(length*2).decode('hex')
-	return s if endianess == 'big' else s[::-1]
-
-def listOverlay(listBase, listAdd, offset):
-	if DEBUG > 1:
-		print("List input:")
-		pprint.pprint(listBase)
-		print("Add input:")
-		pprint.pprint(listAdd)
-
-	for entry in range(0, len(listAdd)):
-		if (entry + offset) >= len(listBase): #safety incase of insanity
-			listBase.append(listAdd[entry])
-		else:
-			listBase[entry + offset] = listAdd[entry]
-
-	if DEBUG > 1:
-		print("Ending list:")
-		pprint.pprint(listBase)
-
-	return listBase
-
-#This is required for all instances, but quite possibly it should be put into a class and inherited.
+# Header data dictionary
 headerOffsets = {
 	"key_start":0,
 	"msg_frag":1,
@@ -75,43 +42,88 @@ headerOffsets = {
 	"sum":10,
 	"key_end":11
 }
-
+# Fragmentation response values
 g_uart_frag_ok = "CT"
 g_uart_frag_bad = "FF"
+# Arduino serial fifo size (-1, since we can't actually fill it up.)
+arduino_frag_size = 63
+
+# isInt
+#
+# @i, type that can be casted to int.
+# @ret, true or false depending on the cast ability.
+def isInt(i):
+	try:
+		int(i)
+		return True
+	except ValueError:
+		return False
+
+# to_bytes
+#
+# @n, integer value
+# @length, size in bytes that the integer should become
+# @endianess, guess.
+def to_bytes(n, length, endianess='big'):
+	h = '%x' % n
+	s = ('0'*(len(h) % 2) + h).zfill(length*2).decode('hex')
+	return s if endianess == 'big' else s[::-1]
+
+# listOverlay
+#
+# @listBase, original list you wish you change.
+# @listAdd, list that you want to put on top of listBase
+# @offset, index offset you want to start listAdd at.
+def listOverlay(listBase, listAdd, offset):
+	if DEBUG > 2:
+		print("listOverlay listBase:")
+		pprint.pprint(listBase)
+		print("listOverlay listAdd:")
+		pprint.pprint(listAdd)
+
+	for entry in range(0, len(listAdd)):
+		if (entry + offset) >= len(listBase): #safety incase of insanity
+			listBase.append(listAdd[entry])
+		else:
+			listBase[entry + offset] = listAdd[entry]
+
+	if DEBUG > 2:
+		print("listOverlay complete list:")
+		pprint.pprint(listBase)
+
+	return listBase
 
 # lrcsum
 #
 # @dataIn, list of ints (Less than 255)
 # @ret, sum output
-#
 def lrcsum(dataIn):
-
 	lrc = 0
 
 	for b in dataIn:
-		if DEBUG > 1:
+		if DEBUG > 3:
+			print("lrcsum data loop: ")
 			pprint.pprint(b)
 
 		lrc ^= struct.unpack('B', str(b))[0]
 
-	if DEBUG > 1:
+	if DEBUG > 3:
 		print("Lrcsum:")
 		pprint.pprint(lrc)
 
 	return to_bytes(lrc, 1, 1)
 
-
+# This is the UART_MH class.  Only one class instance per serial device unless
+# you want to see resource conflicts.
 class UART_MH:
 	def __init__(self):
 		pass
 
+	#This should just get moved into the constructor.
 	def begin(self, serialInterface):
 		if DEBUG:
-			print("UART_MH begin called")
-
-
-		print("Beginning UART_MH INSTANCE!")
-		print(self)
+			print("UART_MH.begin called")
+			print(self)
 
 		self.serialSema = multiprocessing.Semaphore()
 		#Here we define a bunch of class variables
@@ -152,23 +164,19 @@ class UART_MH:
 		for item in self.header:
 			self.headerlen+=self.header[item]
 
-		#self.npmsg = UART_Neopixel()
-
-		#self.ser = serial.Serial(str(serialInterface), self.serialBaud)
 		self.serName = serialInterface
-
-		#At this point we'd be talking to the arduino and getting it's version,
-		# then compare it to the valid ones.
 		self.version = 0x00 #We should sort by largest and select the highest one
 
-		if DEBUG:
-			print("UART_MH Begin complete.")
-
 		self.serialReset()
+
+		if DEBUG:
+			print("UART_MH.begin() complete.")
 
 	#  This method performs a hard open/close of the serial device for 
 	# situations where calling open() just wasn't enough.
 	def serialReset(self):
+		if DEBUG:
+			print("UART_MH.serialReset() begin")
 		if not isinstance(self.ser, serial.Serial):
 			try:
 				self.ser = serial.Serial(str(self.serName), self.serialBaud, timeout=5)
@@ -199,11 +207,16 @@ class UART_MH:
 			print("UART_MH.serialReset() unable to call serial.isOpen().")
 			return -3
 
+		if DEBUG:
+			print("UART_MH.serialReset() complete")
 		return 0
 
 
 	#This prepares the initial message based on the main command type
 	def assembleHeader(self,messageType):
+		if DEBUG:
+			print("UART_MH.assembleHeader() begin")
+
 		outBuf = [
 			self.key_start,
 			b'\x00',	#This is the fragment set, this needs to be set in finishMessage
@@ -219,25 +232,22 @@ class UART_MH:
 			self.key_end,
 		]
 
-		#WE NEED EXCEPTIONS HERE!
-		#As a side effect of the c struct union, we have an endianness problem.  Here and here alone.
-		#listOverlay(outBuf, to_bytes(self.mhcommands[messageType], 2, "little"), 1)
 		listOverlay(outBuf, self.mhcommands[messageType], headerOffsets["cmd_0"]) #Shifted the offset over to accomodate the fragment
 
-		if DEBUG > 0:
-			print("Messagehandler command: %s:" % str(messageType) )
+		if DEBUG > 1:
+			print("UART_MH.assembleHeader(), Messagehandler command: %s:" % str(messageType) )
 			pprint.pprint(self.mhcommands[messageType])
 
+		if DEBUG:
+			print("UART_MH.assembleHeader() complete")
 
-		#At this point, the message is just about as prepared as we can make it
-		# without the class-specific stuff populating the buffer.
 		return outBuf
 
 
 	#Compute the lrcsum for the message
 	def finishMessage(self,curMsg):
 		if DEBUG:
-			print("finishMessage() called.")
+			print("UART_MH.finishMessage() begin")
 
 
 		if len(curMsg) > 64:
@@ -246,38 +256,46 @@ class UART_MH:
 				curMsg[headerOffsets["msg_frag"]] = to_bytes(msgFrags, 1)
 			#If not in range, we'll leave it set to zero.
 
-		if DEBUG:
-			print("CurMSG Data:")
+		if DEBUG > 2:
+			print("UART_MH.finishMessage(), curMSG Data:")
 			pprint.pprint(curMsg)
-			print("curMsg[headerOffsets['sum'] : %s" % str(headerOffsets["sum"]))
+			print("UART_MH.finishMessage(), curMsg[headerOffsets['sum'] : %s" % str(headerOffsets["sum"]))
 		
 		#Provided we don't move the sum to some strange place, this should be fine.
 		curMsg[headerOffsets["sum"]] = lrcsum(curMsg[:headerOffsets["sum"]])
+
+		if DEBUG:
+			print("UART_MH.finishMessage() complete")
+
 		return curMsg
 
 	#wait for uart input to contain expected characters within timeout seconds
 	def UARTWaitIn(self, timeout, expected=5):
+		if DEBUG:
+			print("UART_MH.UARTWaitIn() begin")
+
 		ltimeout = time.time() + timeout
 		if not isinstance(self.ser, serial.Serial):
-			print("statusWait called whithout configured and open serial.")
+			print("UART_MH.UARTWaitIn(), serial instance does not exist.")
 			return -1
 
 		counter = 0
 
-		#try:
-		if True:
+		try:
 			while self.ser.inWaiting() != expected : #While we have no input data
 				if (counter % 1000) == 0:
 					if time.time() > ltimeout:
 						return 1
 				counter+=1
-		#except:
-		else:
-			print("UARTWaitIn failed to read serial interface.")
+		except:
+			print("UART_MH.UARTWaitIn(), failed to read serial interface.")
 			return -1
 
 		if DEBUG:
-			print("Counter broke at %s", str(counter))
+			print("UART_MH.UARTWaitIn(), Counter broke at %s", str(counter))
+
+		if DEBUG:
+			print("UART_MH.UARTWaitIn() end")
 
 		return 0
 
@@ -285,59 +303,53 @@ class UART_MH:
 	#This sends the message
 	def sendMessage(self,buf):
 		if DEBUG:
-			print("Current UART_MH instance ID:")
-			print(self)
-		if isinstance(buf, int):
-			print("sendMessage buffer incomplete.")
-			return 1
+			print("UART_MH.sendMessage(), begin")
 
-		if DEBUG:
-			print("INSIDE SENDMESSAGE NOW")
+		if isinstance(buf, int):
+			print("UART_MH.sendMessage(), buffer incomplete.")
+			return 1
 
 		self.serialSema.acquire()
 
-		#self.ser = serial.Serial(str(serialInterface), self.serialBaud)
 		try:
 			if isinstance(self.ser, serial.Serial):
-#				if self.ser.isOpen(): #ORIG
-#					self.ser.close() #ORIG
 				if not self.ser.isOpen():
 					if self.serialReset():
-						print("Serial reset failed in sendManageMessage, bailing.")
+						print("UART_MH.sendMessage(), Serial reset failed.")
 						self.serialSema.release()
 						return 2
+			else:
+				if self.serialReset():
+					print("UART_MH.sendMessage(), serial create failed.")
+					self.serialSema.release()
+					return 2
 
 		except:
-			print("sendMessage failed when cycling serial interface.")
+			print("UART_MH.sendMessage(), failed when polling serial interface.")
 			self.serialSema.release()
 			return 2
 
-		# try:
-		# 	self.ser = serial.Serial(str(self.serName), self.serialBaud)
-		# except:
-		# 	print("sendMessage failed when opening serial interface.")
-		# 	self.serialSema.release()
-		# 	return 3
-
-
-		if DEBUG:
-			print("sendMessage buffer:")
+		if DEBUG > 2:
+			print("UART_MH.sendMessage() buffer:")
 			pprint.pprint(buf)
 
-		#self.ser.open()
+
 		msgFrag = struct.unpack('B', str(buf[headerOffsets["msg_frag"]]))[0]
+
+		# If we are using fragmentation for this packet series.
 		if (int(msgFrag) > 0):
 			if DEBUG:
-				print("MsgFrag is greater than zero")
+				print("UART_MH.sendMessage(), msgFrag is greater than zero")
 
 			#Split the buffer into msg_frag lists 64 elements in size
-			packetChunks = [ buf[ x:(x+63) ] for x in xrange(0, len(buf), 63) ]
+			packetChunks = [ buf[ x:(x+arduino_frag_size) ] for x in xrange(0, len(buf), arduino_frag_size) ]
 			for chunk in packetChunks:
-				if DEBUG >= 2:
-					print("\n\n^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^")
-					print("MsgFrag Chunk: ")
+				if DEBUG > 2:
+					print("\n")
+					print("//////////////////////////////////////////////////")
+					print("UART_MH.sendMessage(), MsgFrag Chunk: ")
 					pprint.pprint(chunk)
-					print("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^")
+					print("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\")
 
 				chunkComplete = False
 				while not chunkComplete:
@@ -345,108 +357,94 @@ class UART_MH:
 						try:
 							self.ser.write(b)
 						except:
-							print("UART_MH::sendMessage - failed to write to serial interface with fragment.")
+							print("UART_MH.sendMessage(), failed to write to serial interface with fragment.")
 							#return 11
 					state = self.ser.readline()
 					if state.startswith(g_uart_frag_bad):
 						if DEBUG:
-							print("packet chunk send failure, device reports fragment bad.")
+							print("UART_MH.sendMessage(), packet chunk send failure, device reports fragment bad.")
 						time.sleep(0.5)
 					elif state.startswith(g_uart_frag_ok):
 						if DEBUG:
-							print("packet chunk send good!")
+							print("UART_MH.sendMessage(), packet chunk send good.")
 						chunkComplete = True
 						break #We probably don't need the stupid chunkComplete stuff.
 
-		else: #Otherwise we've got a buffer size that's not worth computing
+		# If we are NOT using fragmentation...
+		else:
 			for b in buf:
 				try:
 					self.ser.write(b)
 				except:
-					print("UART_MH::sendMessage - failed to write to serial interface")
+					print("UART_MH.sendMessage(), failed to write to serial interface")
 					self.serialSema.release()
 					return 10
 
-		#Desperately wait for data to be returned from the device.
-		#We dynamically adjust this to the number of output commands
 		if self.UARTWaitIn(4):
-			print("Input data timed out.")
+			print("UART_MH.sendMessage(), input data timed out.")
 			self.serialSema.release()
 			return 4
 
 		try:
 			retd = self.ser.readline()
 		except:
-			print("Failed to readline!")
+			print("UART_MH.sendMessage(), failed to readline (Response data unknown).")
 			self.serialSema.release()
 			return 5
 
 		if DEBUG:
+			print("UART_MH.sendMessage(), readline response data: ")
 			pprint.pprint(retd)
-
-#		try:
-#			self.ser.close()
-#		except:
-#			print("sendMessage failed to close serial interface.")
-#			self.serialSema.release()
-#			return 6
 
 		self.serialSema.release()
 
 		if retd.startswith("ACK"):
 			if DEBUG:
-				print("sendMessage ACK'd")
+				print("UART_MH.sendMessage(), complete (Good)")
 			return 0
+
+		if DEBUG:
+			print("UART_MH.sendMessage(), complete (Bad)")
 
 		return 7
 
-		#Right now, we're using a sleep.  In version 0x01 it'll be a set of 32 0x00's to end a group
-		#time.sleep(0.15)
-
-	#This will continue reading from the serial interface until it retrieves a NAK or an ACK.
-	# It will wait for a maximum of 30 seconds as part of large realistic bounds for configuration.
-	# It should return a raw byte array with the same endianess of the UART stream
-	#This sends the management message (Supposing it is one)
+	# Send a management message request to the firmware.
 	def sendManageMessage(self,buf):
 		if DEBUG:
-			print("SendManageMessage called")
+			print("UART_MH.sendManageMessage() called")
+
 		if isinstance(buf, int):
-			print("sendMessage buffer incomplete.")
+			print("UART_MH.sendManageMessage(), buffer incomplete.")
 			return 1
 
 		self.serialSema.acquire()
 
 		try:
 			if isinstance(self.ser, serial.Serial):
-#				if self.ser.isOpen():	#ORIG
-#					self.ser.close()	#ORIG
 				if not self.ser.isOpen():
 					if self.serialReset():
-						print("Serial reset failed in sendManageMessage, bailing.")
+						print("UART_MH.sendManageMessage(), serial reset failed.")
 						self.serialSema.release()
 						return 2
-						
+			else:
+				if self.serialReset():
+					print("UART_MH.sendManageMessage(), serial create failed.")
+					self.serialSema.release()
+					return 2	
 		except:
-			print("sendManageMessage failed when cycling serial interface.")
+			print("UART_MH.sendManageMessage(), failed when polling serial interface.")
 			self.serialSema.release()
 			return 2
 
-		# try:
-		# 	self.ser = serial.Serial(str(self.serName), self.serialBaud, timeout=5)
-		# except:
-		# 	print("sendManageMessage failed when opening serial interface.")
-		# 	self.serialSema.release()
-		# 	return 3
-
 		if DEBUG > 2:
-			print("sendManageMessage buffer:")
+			print("UART_MH.sendManageMessage(), buffer:")
 			pprint.pprint(buf)
 
 		for b in buf:
 			try:
 				self.ser.write(b)
 			except:
-				print("UART_MH::sendManageMessage - failed to write to serial interface")
+				print("UART_MH.sendManageMessage(), failed to write to serial interface")
 				self.serialSema.release()
 				return 10
 
@@ -460,16 +458,17 @@ class UART_MH:
 			while not self.ser.inWaiting():
 				if (counter % 1000) == 0:
 					if time.time() > ltimeout:
+						print("UART_MH.sendManageMessage(), timeout in first completion loop.")
 						self.serialSema.release()
 						return 1
 				counter+=1
 		except:
-			print("sendManageMessage failed when waiting for message.")
+			print("UART_MH.sendManageMessage(), failed when waiting for first response.")
 			self.serialSema.release()
 			return 4
 
-		if DEBUG:
-			print("sMgmtMsg Counter 1 broke at %s", str(counter))
+		if DEBUG > 2:
+			print("UART_MH.sendManageMessage(), counter break at %s", str(counter))
 
 		ltimeout = time.time() + 30
 		complete = False
@@ -479,7 +478,7 @@ class UART_MH:
 		while not complete:
 			if (counter % 1000) == 0:
 				if time.time() > ltimeout:
-					print("sMgmtMsg timeout 2")
+					print("UART_MH.sendManageMessage(), timeout in second completion loop.")
 					self.serialSema.release()
 					return 5
 
@@ -487,7 +486,7 @@ class UART_MH:
 				while self.ser.inWaiting() > 0:
 					oBuf+=self.ser.read(1)
 			except:
-				print("sendManageMessage failed when waiting for second message completion.")
+				print("UART_MH.sendManageMessage(), failed when waiting for second response.")
 				self.serialSema.release()
 				return 6
 
@@ -500,21 +499,12 @@ class UART_MH:
 			counter+=1
 
 
-		if DEBUG:
+		if DEBUG > 2:
+			print("UART_MH.sendManageMessage(), output buffer: ")
 			pprint.pprint(oBuf)
 
-		try:
-			#self.ser.close() #ORIG
-			pass
-		except:
-			print("sendManageMessage failed when closing serial interface.")
-			self.serialSema.release()
-			return 7
-
-		#Right now, we're using a sleep.  In version 0x01 it'll be a set of 32 0x00's to end a group
-		#time.sleep(0.15)
 		if DEBUG:
-			print("SendManageMessage end")
+			print("UART_MH.sendManageMessage(), complete")
 
 		self.serialSema.release()
 
