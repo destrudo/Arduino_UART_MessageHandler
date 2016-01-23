1d02cab044969e728287e936ad49fe6ca036eb82
Destrudo Dole <destrudo@nulluniverse.com>
2016-01-23 01:58:11 -0800

    updated uartmh_memcheck sketch example to support the digital interface properly.

--- examples/UARTMH_memcheck/UARTMH_memcheck.ino
+++ examples/UARTMH_memcheck/UARTMH_memcheck.ino
@@ -14,21 +14,25 @@
 
 UART_MessageHandler mh = UART_MessageHandler();
 UART_Neopixel uart_np = UART_Neopixel();
+UART_Digital uart_digital = UART_Digital();
 uint16_t bufLen = 0;
-uint8_t printCount = 0;
+uint16_t printCount = 0;
 
 void setup() {
   Serial.begin(115200);
-  Serial1.begin(1000000);
+  Serial1.begin(250000);
   
   mh.setUART(&Serial1);
   mh.configure(&uart_np); /* Configure the UART_Neopixel instance */
+  mh.configure(&uart_digital);
   delay(2000);
   Serial.println("Starting instance...");
 }
 
 void loop() {
   uint8_t status;
+  bufLen = 0;
+  
   if (Serial1.available())
   {
     bufLen = mh.run(status);
@@ -43,12 +47,14 @@ void loop() {
   /* We need to clear any message in the buffer (if allocated) */
   if (bufLen != 0)
   {
+    Serial.print("Outside of loop, bufLen is: ");
+    Serial.println(bufLen);
     mh.clear();
   }
   
   printCount++;
 
-  if (printCount >= 200) {
+  if (printCount >= 20000) {
     Serial.print("Free Memory: '");
     Serial.print(freeMemory());
     Serial.println("'");
